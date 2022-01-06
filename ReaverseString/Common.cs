b733089dd1e434a0eca4094e6620dde7375f91a2
Jayant <jayanttripathy1985@gmail.com>
2022-01-06 10:46:06 +0530

    Add project files.

---
+++ ReaverseString/Common.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReaverseString
+{
+    public static class Common
+    {
+        internal static string ReverseString(string input)
+        {
+            string output = string.Empty;
+            char[] chars = input.ToCharArray();
+            for (int i = chars.Length - 1; i >= 0; i--)
+            {
+                output += chars[i];
+            }
+            return output;
+        }
+    }
+}
---
+++ ReaverseString/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ReaverseString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Type your text and press enter
+            Console.WriteLine("Enter a string that you want to Reverse:");
+
+            // Create a string variable and get user input from the keyboard and store it in the variable
+            string reverse = Console.ReadLine();
+
+            // Print the value of the variable of Reverse function call, which will display the input value
+            Console.WriteLine("Reverse string is: " + Common.ReverseString(reverse));
+
+        }
+    }
+}
